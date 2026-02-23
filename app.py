d52f6356881020013e9dfc9f2755e0af329a36c3
Mohammad Iskandarany <m7md.k.isk@gmail.com>
2026-02-23 14:48:57 +0300

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from a deployed Juju Demo! 🚀"
+    return "Hello world from a deployed Juju Demo! 🚀"
 
 
 if __name__ == "__main__":
